8a35a51d4bf506febd69490f21f3850bc4a24f8e
Daya Souza <dayanalvesouza@gmail.com>
2024-08-26 19:56:53 -0300

    commit inicial

---
+++ ContaBanco/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        
+	    //TODO: Exibir as mensagens para o nosso usuário
+
+	    //TODO: Obter pela classe scanner os valores digitados no terminal
+        
+	    //TODO: Exibir a mensagem da conta criada
+    }
+}
